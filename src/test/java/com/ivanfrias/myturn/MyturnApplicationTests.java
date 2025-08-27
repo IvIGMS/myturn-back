2eef813b89c06cfa6f0a6fdf9ef779d9885ea5b1
ivanfrias <friasgilivan@gmail.com>
2025-08-27 13:21:15 +0200

    feat: comenzamos la configuracion de los tests

--- src/test/java/com/ivanfrias/myturn/MyturnApplicationTests.java
+++ src/test/java/com/ivanfrias/myturn/MyturnApplicationTests.java
@@ -2,8 +2,10 @@ package com.ivanfrias.myturn;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MyturnApplicationTests {
 
 	@Test
---
+++ src/test/java/com/ivanfrias/myturn/users/controllers/common/AbstractControllerTest.java
@@ -0,0 +1,39 @@
+package com.ivanfrias.myturn.users.controllers.common;
+
+import com.ivanfrias.myturn.security.services.JwtService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.impl.DefaultClaims;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.when;
+
+public abstract class AbstractControllerTest {
+
+    protected static final String TOKEN = "faketoken";
+
+    @Mock
+    protected JwtService jwtService;
+
+    @Mock
+    protected HttpServletRequest request;
+
+    protected Claims claims;
+
+    @BeforeEach
+    void setupCommonMocks() {
+
+    }
+
+    protected void mockAuthenticatedUser(Long userId, String role) {
+        claims = new DefaultClaims();
+        claims.put("user_id", userId);
+        if (role != null) {
+            claims.put("role", role);
+        }
+
+        when(request.getHeader("Authorization")).thenReturn("Bearer " + TOKEN);
+        when(jwtService.extractAllClaims(TOKEN)).thenReturn(claims);
+    }
+}
